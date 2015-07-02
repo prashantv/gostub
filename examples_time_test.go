e1c60fc31099b206ca46a22572151408eea08d0a
Prashant Varanasi <prashant@prashantv.com>
2015-07-02 06:44:55 -0700

    Add example for how to stub time

---
+++ examples_time_test.go
@@ -0,0 +1,27 @@
+package gostub_test
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/prashantv/gostub"
+)
+
+// Production code
+var timeNow = time.Now
+
+func GetDay() int {
+	return timeNow().Day()
+}
+
+// Test code
+func Example_stubFunctions() {
+	stubs := gostub.Stub(&timeNow, func() time.Time {
+		return time.Date(2015, 07, 02, 0, 0, 0, 0, time.UTC)
+	})
+	defer stubs.Reset()
+
+	fmt.Printf("Stubbed: %v\n", GetDay())
+	// Output:
+	// Stubbed: 2
+}
